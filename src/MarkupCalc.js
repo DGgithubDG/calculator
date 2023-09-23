acba131e55527de02432bf1d1b4576a4bfbc061c
Portolio <Denisng666@gmail.com>
2023-09-23 21:59:45 +0200

    calc in progress

---
+++ src/MarkupCalc.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Nutrition from './Nutrition'
+
+function MarkupCalc() {
+  return (
+    <div>
+    <h1 className='text-center'> MarkupCalc</h1>
+    <div>
+  {/* <Link to='/Nutrition'>Hello</Link> */}
+</div>
+    </div>
+  )
+}
+
+export default MarkupCalc
