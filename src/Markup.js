f884ad6db47b1e588c4db2b7eebcea9c543d3ae7
Portolio <Denisng666@gmail.com>
2023-09-29 21:52:38 +0200

    commit

--- src/Markup.js
+++ src/Markup.js
@@ -1,10 +1,9 @@
-import React , { useState }from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 function Markup() {
-
   const [costOfFood, setCostOfFood] = useState('');
   const [garnishCost, setGarnishCost] = useState('');
   const [extraCost, setExtraCost] = useState('');
@@ -13,141 +12,161 @@ function Markup() {
   const [grossProfit, setGrossProfit] = useState('');
   const [error, setError] = useState('');
 
+  // Calculate menu price and gross profit when inputs change
+  const calculateProfit = () => {
+    // Convert margin to a decimal (e.g., 50% -> 0.50)
+    const marginDecimal = parseFloat(margin) / 100;
 
-// Calculate menu price and gross profit when inputs change
-const calculateProfit = () => {
-  // Convert margin to a decimal (e.g., 50% -> 0.50)
-  const marginDecimal = parseFloat(margin) / 100;
+    // Calculate the total cost
+    const totalCost = parseFloat(costOfFood) + parseFloat(garnishCost) + parseFloat(extraCost);
 
-  // Calculate the total cost
-  const totalCost = parseFloat(costOfFood) + parseFloat(garnishCost) + parseFloat(extraCost);
+    // Calculate the menu price by adding the profit margin
+    const menuPriceValue = totalCost * (1 + marginDecimal);
 
-  // Calculate the menu price by adding the profit margin
-  // const menuPriceValue = totalCost / (1 - marginDecimal);
-  const menuPriceValue = totalCost * (1 + marginDecimal);
+    // Calculate gross profit
+    const grossProfitValue = menuPriceValue - totalCost;
 
-  // Calculate gross profit
-  const grossProfitValue = menuPriceValue - totalCost;
+    if (!costOfFood || !garnishCost || !extraCost || !margin) {
+      setError('Some fields are empty.');
+      return;
+    }
 
-  if (!costOfFood || !garnishCost || !extraCost || !margin) {
-    setError('Some fields are empty.');
-    return;
-  }
+    // Update the state with the results
+    setMenuPrice(menuPriceValue.toFixed(2));
+    setGrossProfit(grossProfitValue.toFixed(2));
+    setError('');
+  };
 
-  // Update the state with the results
-  setMenuPrice(menuPriceValue.toFixed(2));
-  setGrossProfit(grossProfitValue.toFixed(2));
-  setError('');
+  // Function to handle Enter key press
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevent form submission
+      calculateProfit();
+    }
+  };
 
-};
+  // Add event listener for Enter key press when the component mounts
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 
-  
   return (
     <div>
-    <Navbar />
-    <h1 className='text-center'>Selling Cost Page</h1>
+      <Navbar />
+      <h1 className='text-center'>Selling Cost Page</h1>
 
-    <div className="a container">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-6">
-          <div className="card">
-            <div className="card-header">
-              <h5 className='text-center'><b>Markup Calculator</b></h5>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-6">
+            <div className="card">
+              <div className="card-header">
+                <h5 className='text-center'><b>Markup Calculator</b></h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <div className="row">
+                    <div className="col">
+                      <p>Cost Of Food</p>
+                    </div>
+                    <div className="col text-end">
+                      <input
+                        type="number"
+                        placeholder='0.00'
+                        value={costOfFood}
+                        onChange={(e) => setCostOfFood(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="row">
+                    <div className="col">
+                      <p>Garnish Cost</p>
+                    </div>
+                    <div className="col text-end">
+                      <input
+                        type="number"
+                        placeholder='0.00'
+                        value={garnishCost}
+                        onChange={(e) => setGarnishCost(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="row">
+                    <div className="col">
+                      <p>Extra Cost</p>
+                    </div>
+                    <div className="col text-end">
+                      <input
+                        type="number"
+                        placeholder='0.00'
+                        value={extraCost}
+                        onChange={(e) => setExtraCost(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className="row">
+                    <div className="col">
+                      <p>Desired Margin %</p>
+                    </div>
+                    <div className="col text-end">
+                      <input
+                        type="number"
+                        placeholder='0 %'
+                        value={margin}
+                        onChange={(e) => setMargin(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+                </li>
+                <li className="list-group-item text-center">
+                  <button
+                    className="btn btn-secondary"
+                    onClick={calculateProfit}
+                    style={{ width: "60%" }}
+                  >
+                    Calculate
+                  </button>
+                  {error && <p className="error-message">{error}</p>}
+                </li>
+                <li className="list-group-item">
+                  <div className="row">
+                    <div className="col">
+                      <p>Menu Price</p>
+                    </div>
+                    <div className="col text-end">
+                      {menuPrice}
+                    </div>
+                  </div>
+                </li>
+                <li className="list-group-item">
+                  <div className='row'>
+                    <div className='col'>
+                      <p>Gross Profit</p>
+                    </div>
+                    <div className='col text-end'>
+                      {grossProfit}
+                    </div>
+                  </div>
+                </li>
+              </ul>
             </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                <div className="row">
-                  <div className="col">
-                    <p>Cost Of Food</p>
-                  </div>
-                  <div className="col text-end">
-                    <input
-                      type="number"
-                      placeholder='0.00'
-                      value={costOfFood}
-                      onChange={(e) => setCostOfFood(e.target.value)}
-                      className="form-control"
-                    />
-                  </div>
-                </div>
-              </li>
-              <li className="list-group-item">
-                <div className="row">
-                  <div className="col">
-                    <p>Garnish Cost</p>
-                  </div>
-                  <div className="col text-end">
-                    <input
-                      type="number"
-                      placeholder='0.00'
-                      value={garnishCost}
-                      onChange={(e) => setGarnishCost(e.target.value)}
-                      className="form-control"
-                    />
-                  </div>
-                </div>
-              </li>
-              <li className="list-group-item">
-                <div className="row">
-                  <div className="col">
-                    <p>Extra Cost</p>
-                  </div>
-                  <div className="col text-end">
-                    <input
-                      type="number"
-                      placeholder='0.00'
-                      value={extraCost}
-                      onChange={(e) => setExtraCost(e.target.value)}
-                      className="form-control"
-                    />
-                  </div>
-                </div>
-              </li>
-              <li className="list-group-item">
-                <div className="row">
-                  <div className="col">
-                    <p>Desired Margin %</p>
-                  </div>
-                  <div className="col text-end">
-                    <input
-                      type="number"
-                      placeholder='0 %'
-                      value={margin}
-                      onChange={(e) => setMargin(e.target.value)}
-                      className="form-control"
-                    />
-                  </div>
-                </div>
-              </li>
-              <li className="list-group-item text-center">  <button className='btn btn-secondary' onClick={calculateProfit}  style={{width: "60%"}}>Calculate</button>
-      {error && <p className="error-message">{error}</p>}</li>
-              <li className="list-group-item">
-                <div className="row">
-                  <div className="col">
-                    <p>Menu Price</p>
-                  </div>
-                  <div className="col text-end">
-                    {menuPrice}
-                  </div>
-                </div>
-              </li>
-              <li className="list-group-item">
-                <div className='row'>
-                  <div className='col'>
-                    <p>Gross Profit</p>
-                  </div>
-                  <div className='col text-end'>
-                    {grossProfit}
-                  </div>
-                </div>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,28 +6,30 @@ function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
+  <Link to='/'style={{ textDecoration: 'none'  }}>
     <a className="navbar-brand" href="#">DG.</a>
+    </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <Link to='/markup'>
+        <Link to='/markup'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
           <a className="nav-link active"  aria-current="page"  style={{ textDecoration: 'none'  }} >Markup</a>
         </li>
         </Link>
-        <Link to='/dishcost'>
+        <Link to='/dishcost'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#"> Dish Cost</a>
         </li>
         </Link>
-        <Link to='/conversion'>
+        <Link to='/conversion'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#"> Conversion</a>
         </li>
         </Link>
-        <Link to='/nutrition'>
+        <Link to='/nutrition'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
           <a className="nav-link active"  aria-current="page"  style={{ textDecoration: 'none'  }} >Nutrition</a>
         </li>
