23385c46542c94b9b1835ff402666bd30172949a
Portolio <Denisng666@gmail.com>
2023-09-30 22:59:51 +0200

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,36 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const gifUrl = 'https://media3.giphy.com/media/aRZ4vTsHnyW6A/giphy.gif';
+
+  const backgroundImageStyle = {
+    backgroundImage: `url(${gifUrl})`, // Set the background image URL
+  };
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" style={backgroundImageStyle}>
   <div className="container-fluid">
   <Link to='/'style={{ textDecoration: 'none'  }}>
-    <a className="navbar-brand" href="#">DG.</a>
+    <a className="navbar-brand" href="#" style={{  color: 'white' }}>DG.</a>
     </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon custom-toggler-icon" ></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <Link to='/markup'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
-          <a className="nav-link active"  aria-current="page"  style={{ textDecoration: 'none'  }} >Markup</a>
+        <a className="nav-link active" aria-current="page" style={{  color: 'white' }}>Markup</a>
         </li>
         </Link>
         <Link to='/dishcost'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#"> Dish Cost</a>
+          <a className="nav-link active" aria-current="page" href="#" style={{  color: 'white' }} > Dish Cost</a>
         </li>
         </Link>
         <Link to='/conversion'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#"> Conversion</a>
+          <a className="nav-link active" aria-current="page" href="#" style={{  color: 'white' }}> Conversion</a>
         </li>
         </Link>
         <Link to='/nutrition'style={{ textDecoration: 'none'  }}>
         <li className="nav-item">
-          <a className="nav-link active"  aria-current="page"  style={{ textDecoration: 'none'  }} >Nutrition</a>
+          <a className="nav-link active"  aria-current="page"  style={{  color: 'white' }} >Nutrition</a>
         </li>
         </Link>
       </ul>
